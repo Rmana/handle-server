7f5099a4e65d0011784e480d79c76d7340b319a8
Howard Ding <hding2@illinois.edu>
2012-10-10 12:24:50 -0500

    initial commit

---
+++ lib/handle-server.rb
@@ -0,0 +1,7 @@
+require "handle-server/version"
+
+module Handle
+  module Server
+    # Your code goes here...
+  end
+end
---
+++ lib/handle-server/version.rb
@@ -0,0 +1,5 @@
+module Handle
+  module Server
+    VERSION = "0.0.1"
+  end
+end
